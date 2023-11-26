dab0d658567ef245a694beecdf9b309ecc02a4b6
mark ramy <markramyy@gmail.com>
2023-11-26 13:56:08 +0200

    Adding tenants models and apply modification in the settings

--- config/settings.py
+++ config/settings.py
@@ -142,3 +142,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+TENANT_MODEL = "tenants.Tenant"
+
+TENANT_DOMAIN_MODEL = "tenants.Domain"
--- tenants/models.py
+++ tenants/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django_tenants.models import TenantMixin, DomainMixin
 
 # Create your models here.
+class Tenant(TenantMixin):
+    name = models.CharField(max_length=100)
+    created_on = models.DateField(auto_now_add=True)
+    updated_on = models.DateField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Domain(DomainMixin):
+    pass
