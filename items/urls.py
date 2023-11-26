de4975ca514f72d5bbef1b50fc37fdc0bf35a72e
mark ramy <markramyy@gmail.com>
2023-11-26 17:53:15 +0200

    Creating serializers and api for the swagger documentation for items

--- items/urls.py
+++ items/urls.py
@@ -1,7 +1,16 @@
-from django.urls import path
+from django.urls import path,include
 
 from .views import ItemListView, ItemDetailView, ItemCreateView, ItemUpdateView, ItemDeleteView
+
+from rest_framework.routers import DefaultRouter
+from .api import ItemViewSet
+
+router = DefaultRouter()
+router.register('items', ItemViewSet, basename='item')
+
 urlpatterns = [
+    path('api/', include(router.urls)),
+
     path('', ItemListView.as_view(), name='item_list'),
     path('<int:pk>/', ItemDetailView.as_view(), name='item_detail'),
     path('create/', ItemCreateView.as_view(), name='item_create'),
