b5ca6b27c9cab013ee07cde794a34a36ed9d3870
mark ramy <markramyy@gmail.com>
2023-11-26 14:11:06 +0200

    Adding base views for items and Adjust the urls accord it

--- items/views.py
+++ items/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
+from django.http import HttpResponse
+
 # Create your views here.
+def index(request):
+    return HttpResponse(f"<h1>{request.tenant} Items</h1>")
