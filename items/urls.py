9a218d363fe083b5db797c84a5cb6ddfda1c51fd
mark ramy <markramyy@gmail.com>
2023-11-26 14:55:31 +0200

    Changing items view and apply these changes to the urls and creating template for it

--- items/urls.py
+++ items/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from .views import index
+from .views import item_list, item_detail, item_create
 
 urlpatterns = [
-    path('', index, name='index'),
+    path('', item_list, name='item_list'),
+    path('<int:id>/', item_detail, name='item_detail'),
+    path('create/', item_create, name='item_create'),
 ]
--- items/views.py
+++ items/views.py
@@ -1,7 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-from django.http import HttpResponse
+from .models import Item
 
 # Create your views here.
-def index(request):
-    return HttpResponse(f"<h1>{request.tenant} Items</h1>")
+def item_list(request):
+    items = Item.objects.all()
+    return render(request, 'items/item_list.html', {'items': items})
+
+def item_detail(request, id):
+    item = Item.objects.get(id=id)
+    return render(request, 'items/item_detail.html', {'item': item})
+
+def item_create(request, name, description, price):
+    if request.POST:
+        name = request.POST.get('name')
+        description = request.POST.get('description')
+        price = request.POST.get('price')
+        Item.objects.create(name=name, description=description, price=price)
+        return redirect('item_list')
+
